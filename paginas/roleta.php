c3ddd55714f737b7254d1c40ba109d20ea714bf7
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2025-06-20 09:38:40 -0300

    organizacao de pastas

--- paginas/roleta.php
+++ paginas/roleta.php
@@ -2,7 +2,7 @@
 		if(isset($_POST["roleta"])){
 			$aluguel = rand(0,300);
 			echo "<h2>O valor do Aluguel é: " . $aluguel . "</h2>";
-			echo "retornando a página inicial";
-			header("refresh: 3; index.php");
+			echo "retornando a página inicial em 3 segundos";
+			header("refresh: 3; ./../index.php");
 		}
 ?> 
